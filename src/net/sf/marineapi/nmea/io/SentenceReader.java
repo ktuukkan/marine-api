5166a328f3c034a9fbe13e39ad90bfa3a0ab5eed
Kimmo Tuukkanen <ktuukkan@users.sourceforge.net>
2010-03-10 19:15:11 +0000

    Refactored SentenceReader, some improvements here and there.

--- src/net/sf/marineapi/nmea/io/SentenceReader.java
+++ src/net/sf/marineapi/nmea/io/SentenceReader.java
@@ -21,7 +21,8 @@
 package net.sf.marineapi.nmea.io;
 
 import java.io.BufferedReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
@@ -31,46 +32,24 @@ import net.sf.marineapi.nmea.event.SentenceListener;
 import net.sf.marineapi.nmea.parser.Sentence;
 
 /**
- * An abstract sentence reader class. You may extend it to create your own
- * readers to read NMEA from different data sources (ports, files etc.).
+ * Sentence provider reads NMEA sentences from the specified InputStream and
+ * dispatches them to listeners as SentenceEvents.
  * 
  * @author Kimmo Tuukkanen
  * @version $Revision$
  */
-@Deprecated
-public abstract class SentenceReader extends Thread {
+public class SentenceReader {
 
-    // Thread's update rate in milliseconds.
-    private int delay = 100;
-
-    // Time to wait before receive flag is set to false.
-    private int timeout = 3000;
-
-    // Time stamp for last NMEA sentence
-    private long lastUpdate = 0;
-
-    // BufferedReader for NMEA input.
-    protected BufferedReader in = null;
-
-    // PrintWriter for NMEA output.
-    protected PrintWriter out = null;
-
-    // Flag for indicating if reader is receiving NMEA
-    private boolean receiving = false;
-
-    // Flag that keeps thread loop running
-    private boolean running = true;
-
-    // Vector for SentenceListeners
+    // Vector (synchronized) for SentenceListeners
     private final List<SentenceListener> listeners = new Vector<SentenceListener>();
-
-    // Buffer for writing to data source
-    private final List<String> writeBuffer = new Vector<String>();
+    private Worker worker;
 
     /**
      * Constructor.
      */
-    public SentenceReader() {
+    public SentenceReader(InputStream source) {
+        worker = new Worker(new BufferedReader(new InputStreamReader(source)));
+        new Thread(worker).start();
     }
 
     /**
@@ -87,71 +66,13 @@ public abstract class SentenceReader extends Thread {
     }
 
     /**
-     * Makes a clean exit from <code>run()</code> and closes input and output
-     * streams. The <code>disconnect()</code> method is then invoked to close
-     * the actual data source (eg. serial port).
-     */
-    public final void close() {
-        receiving = false;
-        running = false;
-        interrupt();
-
-        try {
-            in.close();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        disconnect();
-    }
-
-    /**
-     * Close communications with the data source. Implementation of this method
-     * depends on the type of the NMEA source (serial port, socket, file etc).
-     */
-    protected abstract void disconnect();
-
-    /**
-     * Dispatch data to all listeners.
-     * 
-     * @param nmea sentence string.
-     */
-    private final void fireSentenceRead(String data) {
-        try {
-            SentenceEvent se = new SentenceEvent(this, data);
-            lastUpdate = System.currentTimeMillis();
-            receiving = true;
-
-            for (SentenceListener sl : listeners) {
-                sl.sentenceRead(se);
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            receiving = false;
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-        }
-    }
-
-    /**
      * Returns an unmodifiable list of <code>SentenceListeners</code> assigned
      * to this reader.
      * 
      * @return List of SentenceListeners.
      */
-    public final List<SentenceListener> getListeners() {
+    public List<SentenceListener> getListeners() {
         return Collections.unmodifiableList(listeners);
-    }
-
-    /**
-     * Tells if the reader is receiving NMEA data.
-     * 
-     * @return <code>true</code>, if new data has been received within the set
-     *         timeout, otherwise <code>false</code>.
-     */
-    public boolean isReceiving() {
-        return receiving;
     }
 
     /**
@@ -166,105 +87,62 @@ public abstract class SentenceReader extends Thread {
     }
 
     /**
-     * Main processing method.
+     * Dispatch data to all listeners.
+     * 
+     * @param nmea sentence string.
      */
-    @Override
-    public void run() {
-        String data = "";
-
-        while (running) {
+    private void fireSentenceRead(Sentence s) {
+        SentenceEvent se = new SentenceEvent(this, s);
+        for (SentenceListener sl : getListeners()) {
             try {
-                // Send oldest message from the output buffer
-                if (writeBuffer.size() > 0) {
-                    data = writeBuffer.get(0) + Sentence.TERMINATOR;
-                    out.print(data);
-                    out.flush();
-                    writeBuffer.remove(0);
-                }
-
-                // Read data if available and dispatch to listeners
-                if (in.ready()) {
-                    data = in.readLine();
-                    this.fireSentenceRead(data);
-                }
-
-                // Check reader receive status
-                if ((System.currentTimeMillis() - lastUpdate) >= timeout) {
-                    receiving = false;
-                }
-
-                sleep(delay);
-
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-
+                sl.sentenceRead(se);
             } catch (Exception e) {
-                e.printStackTrace();
-                close();
+                // nevermind listener failures
             }
         }
     }
 
     /**
-     * Sets the reader update rate in milliseconds. For some data sources, files
-     * for example, you may want to slow down the reader. The default value is
-     * 100 ms (0.1 sec) and values smaller than 50 ms are ignored.
-     * 
-     * @param millis Thread sleep time in milliseconds (minimum 50 ms).
+     * Reads the input stream and fires sentence events.
      */
-    public void setUpdateRate(int millis) {
-        if (millis >= 50) {
-            delay = millis;
+    private class Worker implements Runnable {
+
+        private boolean running;
+        private BufferedReader in;
+
+        /**
+         * Constructor
+         */
+        public Worker(BufferedReader buf) {
+            in = buf;
+            running = true;
+        }
+
+        /**
+         * Reads the input stream and fires SentenceEvents
+         */
+        public void run() {
+            String data = null;
+            while (running) {
+                try {
+                    if (in.ready()) {
+                        data = in.readLine();
+                        if (Sentence.isValid(data)) {
+                            fireSentenceRead(new Sentence(data));
+                        }
+                    }
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * Stop the run loop.
+         */
+        public void stop() {
+            this.running = false;
         }
     }
-
-    /**
-     * Get the current reader update rate.
-     * 
-     * @return Update rate in milliseconds.
-     * @see #setUpdateRate(int)
-     */
-    public int getUpdateRate() {
-        return delay;
-    }
-
-    /**
-     * Sets the reader timeout in milliseconds. When the set time has exceeded,
-     * the receive flag is set to <code>false</code>. The default value is 3000
-     * ms (3 sec) and values smaller than 1000 ms are ignored.
-     * 
-     * @param millis Receive timeout in milliseconds (minimum 1000 ms).
-     * @see #isReceiving()
-     */
-    public void setReceiveTimeout(int millis) {
-        if (millis >= 1000) {
-            timeout = millis;
-        }
-    }
-
-    /**
-     * Get the current receive timeout.
-     * 
-     * @return Timeout in milliseconds.
-     * @see #setReceiveTimeout(int)
-     * @see #isReceiving()
-     */
-    public int getReceiveTimeout() {
-        return timeout;
-    }
-
-    /**
-     * Writes a String to data source. Sentence is first added to write buffer
-     * which is unloaded by thread loop message-by-message. Because of this it
-     * may take a while to get the message go through the reader, depending on
-     * the current write buffer load.
-     * 
-     * @param s String to be sent to data source.
-     */
-    public void write(String s) {
-        if (s != null && !"".equals(s)) {
-            writeBuffer.add(s);
-        }
-    }
-
 }
