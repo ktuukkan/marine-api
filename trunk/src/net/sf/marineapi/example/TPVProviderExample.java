d98caa02ab2f9cc963f4a532a963103b4d51300e
Kimmo Tuukkanen <ktuukkan@users.sourceforge.net>
2011-07-30 17:09:16 +0000

    Added missing file comments.

--- trunk/src/net/sf/marineapi/example/TPVProviderExample.java
+++ trunk/src/net/sf/marineapi/example/TPVProviderExample.java
@@ -1,3 +1,23 @@
+/* 
+ * TPVProviderExample.java
+ * Copyright (C) 2011 Kimmo Tuukkanen
+ * 
+ * This file is part of Java Marine API.
+ * <http://sourceforge.net/projects/marineapi/>
+ * 
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.example;
 
 import java.io.File;
