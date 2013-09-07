bc7e02d10f78a4960d1b2b4cb065d286b13fa8db
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2013-09-07 12:28:37 +0300

    Issue #2: made sentence validation less strict by allowing any sentence length. Also changed allowed length of sentence ids from exactly 5 to 3-10 chars for proprietary sentences to pass validation.

--- src/main/java/net/sf/marineapi/nmea/sentence/SentenceValidator.java
+++ src/main/java/net/sf/marineapi/nmea/sentence/SentenceValidator.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * SentenceValidator.java
  * Copyright (C) 2010 Kimmo Tuukkanen
- * 
+ *
  * This file is part of Java Marine API.
  * <http://ktuukkan.github.io/marine-api/>
- * 
+ *
  * Java Marine API is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * Java Marine API is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,33 +23,43 @@ package net.sf.marineapi.nmea.sentence;
 import java.util.regex.Pattern;
 
 /**
- * SentenceValidator checks any String against NMEA 0183 format.
- * 
+ * SentenceValidator for detecting and validation of sentence Strings.
+ *
  * @author Kimmo Tuukkanen
  */
 public final class SentenceValidator {
 
 	private static final Pattern reChecksum = Pattern.compile(
-		"^[$|!]{1}[A-Z0-9]{5}[,][\\x20-\\x7F]{0,72}[*][A-F0-9]{2}$");
-	
+		"^[$|!]{1}[A-Z0-9]{3,10}[,][\\x20-\\x7F]+[*][A-F0-9]{2}$");
+
 	private static final Pattern reNoChecksum = Pattern.compile(
-		"^[$|!]{1}[A-Z0-9]{5}[,][\\x20-\\x7F]{0,75}$");
+		"^[$|!]{1}[A-Z0-9]{3,10}[,][\\x20-\\x7F]+$");
 
 	private SentenceValidator() {
 	}
 
 	/**
-	 * Tells if the specified String matches the NMEA 0183 sentence format.
+	 * <p>
+	 * Tells if the specified String matches the NMEA 0183 sentence format.</p>
 	 * <p>
 	 * String is considered as a sentence if it meets the following criteria:
 	 * <ul>
-	 * <li>Starts with '$' character
-	 * <li>'$' is followed by 5 upper-case chars and a comma (sentence type id)
-	 * <li>Length is max. 80 chars long (excluding &lt;CR&gt;&lt;LF&gt;)
-	 * <li>String contains only printable ASCII characters
-	 * <li>Checksum is correct and separated by '*' char (unless omitted)
+	 * <li>First character is '$' or '!'
+	 * <li>Begin char is followed by upper-case sentence ID (3 to 10 chars)
+	 * <li>Sentence ID is followed by a comma and an arbitrary number of
+	 *     printable ASCII characters (payload data)
+	 * <li>Data is followed by '*' and a two-char hex checksum (may be omitted)
 	 * </ul>
-	 * 
+	 * </p>
+	 * <p>
+	 * Notice that format matching is not strict; although NMEA 0183 defines a
+	 * maximum length of 80 chars, the sentence length is not checked. This is
+	 * due to fact that it seems quite common that devices violate this rule,
+	 * some perhaps deliberately, some by mistake. Thus, assuming the formatting
+	 * is otherwise valid, it is not feasible to strictly validate length and
+	 * discard sentences that just exceed the 80 chars limit.
+	 * </p>
+	 *
 	 * @param nmea String to inspect
 	 * @return true if recognized as sentence, otherwise false.
 	 */
@@ -69,9 +79,9 @@ public final class SentenceValidator {
 	/**
 	 * Tells if the specified String is a valid NMEA 0183 sentence. String is
 	 * considered as valid sentence if it passes the {@link #isSentence(String)}
-	 * test and contains a valid checksum. Sentences without checksum are
+	 * test and contains correct checksum. Sentences without checksum are
 	 * validated only by checking the general sentence characteristics.
-	 * 
+	 *
 	 * @param nmea String to validate
 	 * @return <code>true</code> if valid, otherwise <code>false</code>.
 	 */
--- src/test/java/net/sf/marineapi/nmea/parser/SentenceValidatorTest.java
+++ src/test/java/net/sf/marineapi/nmea/parser/SentenceValidatorTest.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * SentenceValidatorTest.java
  * Copyright (C) 2010 Kimmo Tuukkanen
- * 
+ *
  * This file is part of Java Marine API.
  * <http://ktuukkan.github.io/marine-api/>
- * 
+ *
  * Java Marine API is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * Java Marine API is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -45,7 +45,7 @@ public class SentenceValidatorTest {
 		assertTrue(SentenceValidator.isValid(a));
 		assertTrue(SentenceValidator.isValid(Checksum.add(a)));
 
-		// '!' as begin char
+		// '!' begin char
 		String b = "!ABCDE,1,2,3,4,5,6,7,8,9";
 		assertTrue(SentenceValidator.isValid(b));
 		assertTrue(SentenceValidator.isValid(Checksum.add(b)));
@@ -76,8 +76,8 @@ public class SentenceValidatorTest {
 		assertFalse(SentenceValidator.isValid("$GpGGA,1,2,3,4,5,6,7,8,9"));
 		assertFalse(SentenceValidator.isValid("$GPGGa,1,2,3,4,5,6,7,8,9"));
 		assertFalse(SentenceValidator.isValid("$GPGG#,1,2,3,4,5,6,7,8,9"));
-		assertFalse(SentenceValidator.isValid("$GPGG,1,2,3,4,5,6,7,8,9"));
-		assertFalse(SentenceValidator.isValid("$GPGGAA,1,2,3,4,5,6,7,8,9"));
+		assertFalse(SentenceValidator.isValid("$AB,1,2,3,4,5,6,7,8,9"));
+		assertFalse(SentenceValidator.isValid("$ABCDEFGHIJK,1,2,3,4,5,6,7,8,9"));
 		assertFalse(SentenceValidator.isValid("$GPGGA,1,2,3,4,5,6,7,8,9*00"));
 	}
 
@@ -101,4 +101,17 @@ public class SentenceValidatorTest {
 		assertTrue(SentenceValidator.isValid(ZDATest.EXAMPLE));
 	}
 
+	@Test
+	public void testIsValidWithLongProprietaryId() {
+		String str = "$PRWIILOG,GGA,A,T,1,0";
+		assertTrue(SentenceValidator.isSentence(str));
+		assertTrue(SentenceValidator.isValid(str));
+	}
+
+	@Test
+	public void testIsValidWithShortProprietaryId() {
+		String str = "$PUBX,03,GT{,ID,s,AZM,EL,SN,LK},";
+		assertTrue(SentenceValidator.isSentence(str));
+		assertTrue(SentenceValidator.isValid(str));
+	}
 }
