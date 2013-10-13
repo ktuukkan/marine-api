19c8dca08acca4edbb89a3f175f7c1d1bc74a1e5
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2013-10-13 18:21:40 +0300

    Added missing file comment in HeadingProvider.

--- src/main/java/net/sf/marineapi/provider/HeadingProvider.java
+++ src/main/java/net/sf/marineapi/provider/HeadingProvider.java
@@ -1,5 +1,22 @@
-/**
- * 
+/*
+ * HeadingProvider.java
+ * Copyright (C) 2012 Kimmo Tuukkanen
+ *
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ *
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
  */
 package net.sf.marineapi.provider;
 
@@ -12,7 +29,7 @@ import net.sf.marineapi.provider.event.HeadingEvent;
 /**
  * Heading provider reports the vessel's current heading. Data is captured from
  * HDT, HDM or HDG sentences.
- * 
+ *
  * @author Kimmo Tuukkanen
  * @see net.sf.marineapi.provider.event.HeadingEvent
  * @see net.sf.marineapi.provider.event.HeadingListener
@@ -21,7 +38,7 @@ public class HeadingProvider extends AbstractProvider<HeadingEvent> {
 
 	/**
 	 * Creates a new intance of HeadingProvider.
-	 * 
+	 *
 	 * @param reader Reader for capturing heading sentences.
 	 */
 	public HeadingProvider(SentenceReader reader) {
