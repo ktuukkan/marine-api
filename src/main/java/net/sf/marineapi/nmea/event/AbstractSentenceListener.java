a06081833fb36181798633ae32ffd499edebaec0
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2014-01-18 14:05:42 +0200

    Added file comment in AbstractSentenceListener.

--- src/main/java/net/sf/marineapi/nmea/event/AbstractSentenceListener.java
+++ src/main/java/net/sf/marineapi/nmea/event/AbstractSentenceListener.java
@@ -1,3 +1,23 @@
+/* 
+ * AbstractSentenceListener.java
+ * Copyright (C) 2014 Kimmo Tuukkanen
+ * 
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ * 
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.nmea.event;
 
 import java.lang.reflect.ParameterizedType;
@@ -14,7 +34,7 @@ import net.sf.marineapi.nmea.sentence.Sentence;
  * <p>
  * Methods of {@link SentenceListener} interface implemented by this class are
  * empty, except {@link #sentenceRead(SentenceEvent)} which detects the incoming
- * sentence parsers and casts them to correct sentence interface before calling
+ * sentence parsers and casts them to correct interface before calling
  * {@link #sentenceRead(Sentence)} method.
  *
  * @author Kimmo Tuukkanen
