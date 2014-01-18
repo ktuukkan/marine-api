af07aec8b788fb8ba133d13784402bcf37d92fc9
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2014-01-18 14:00:03 +0200

    FaaMode javadocs

--- src/main/java/net/sf/marineapi/nmea/util/FaaMode.java
+++ src/main/java/net/sf/marineapi/nmea/util/FaaMode.java
@@ -21,13 +21,13 @@
 package net.sf.marineapi.nmea.util;
 
 /**
- * FaaMode defines the FAA operating modes reported by APB, BWC, BWR, GLL, RMA,
- * RMB, RMC, VTG, WCV, and XTE sentences since NMEA 2.3. Also, the mode field in
- * GGA was extended to contain these statuses.
+ * <p>FAA operating modes reported by APB, BWC, BWR, GLL, RMA, RMB, RMC, VTG,
+ * WCV and XTE sentences since NMEA 2.3. Also, the mode field in GGA was
+ * extended to contain these statuses.</p>
  * <p>
  * Notice that FAA mode dominates the {@link DataStatus} fields. Status field
  * will be set to {@link DataStatus#ACTIVE} for modes {@link #AUTOMATIC} and
- * {@link #DGPS}, and {@link DataStatus#VOID} for all other modes.
+ * {@link #DGPS}, and {@link DataStatus#VOID} for all other modes.</p>
  * 
  * @author Kimmo Tuukkanen
  * @see GpsFixQuality
