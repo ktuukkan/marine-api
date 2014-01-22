5ebc44a3d08586ade89c746a947af2ce1c2b126a
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2014-01-22 22:52:27 +0200

    Added file copyright notes and javadocs.

--- src/main/java/net/sf/marineapi/nmea/parser/ROTParser.java
+++ src/main/java/net/sf/marineapi/nmea/parser/ROTParser.java
@@ -1,3 +1,23 @@
+/*
+ * ROTParser.java
+ * Copyright (C) 2014 Mike Tamis
+ *
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ *
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.nmea.parser;
 
 import net.sf.marineapi.nmea.sentence.ROTSentence;
@@ -5,7 +25,9 @@ import net.sf.marineapi.nmea.sentence.SentenceId;
 import net.sf.marineapi.nmea.sentence.TalkerId;
 
 /**
- * Created by SJK on 22/01/14.
+ * ROT sentence parser.
+ *
+ * @author Mike Tamis
  */
 public class ROTParser extends SentenceParser implements ROTSentence {
 
--- src/main/java/net/sf/marineapi/nmea/sentence/RateOfTurnSentence.java
+++ src/main/java/net/sf/marineapi/nmea/sentence/RateOfTurnSentence.java
@@ -1,7 +1,29 @@
+/*
+ * RateOfTurnSentence.java
+ * Copyright (C) 2014 Mike Tamis
+ *
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ *
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.nmea.sentence;
 
 /**
- * Created by SJK on 22/01/14.
+ * Common Rate of Turn interface.
+ *
+ * @author Mike Tamis
  */
 public interface RateOfTurnSentence extends Sentence {
 
