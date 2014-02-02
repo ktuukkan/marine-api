000ad9535a10b8400a7a7457ab8b6291c173cfb7
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2014-02-02 21:40:42 +0200

    More tests for SentenceReader.

--- src/test/java/net/sf/marineapi/nmea/io/SentenceReaderTest.java
+++ src/test/java/net/sf/marineapi/nmea/io/SentenceReaderTest.java
@@ -1,6 +1,10 @@
 package net.sf.marineapi.nmea.io;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
@@ -9,6 +13,10 @@ import java.io.InputStream;
 
 import net.sf.marineapi.nmea.event.SentenceEvent;
 import net.sf.marineapi.nmea.event.SentenceListener;
+import net.sf.marineapi.nmea.parser.BODTest;
+import net.sf.marineapi.nmea.parser.GGATest;
+import net.sf.marineapi.nmea.parser.SentenceFactory;
+import net.sf.marineapi.nmea.sentence.Sentence;
 import net.sf.marineapi.nmea.sentence.SentenceId;
 
 import org.junit.Before;
@@ -17,32 +25,32 @@ import org.junit.Test;
 
 public class SentenceReaderTest {
 
-	private SentenceReader reader;
-	private SentenceListener listener;
+	public final static String TEST_DATA =
+		"src/test/resources/data/Navibe-GM720.txt";
 	
+	private Sentence sentence;
+	private SentenceReader reader;
+	private SentenceListener dummyListener;
+	private SentenceListener testListener;
+	private boolean paused;
+	private boolean started;
+	private boolean stopped;
+
 	@Before
 	public void setUp() throws Exception {
-		File file = new File("src/test/resources/data/Navibe-GM720.txt");
+		File file = new File(TEST_DATA);
 		InputStream stream = new FileInputStream(file);
 		reader = new SentenceReader(stream);
-		listener = new DummyListener();
-		reader.addSentenceListener(listener);
-	}
-
-	@Test
-	public void testSentenceReaderInputStream() {
-
-	}
-
-	@Test
-	public void testAddSentenceListenerSentenceListenerSentenceId() {
-		DummyListener dummy = new DummyListener();
-		reader.addSentenceListener(dummy, SentenceId.GGA);
+		
+		dummyListener = new DummyListener();
+		testListener = new TestListener();
+		reader.addSentenceListener(dummyListener);
+		reader.addSentenceListener(testListener, SentenceId.GGA);
 	}
 
 	@Test
 	public void testAddSentenceListenerSentenceListenerString() {
-		DummyListener dummy = new DummyListener();	
+		DummyListener dummy = new DummyListener();
 		reader.addSentenceListener(dummy, "GLL");
 	}
 
@@ -53,9 +61,10 @@ public class SentenceReaderTest {
 
 	@Test
 	public void testRemoveSentenceListener() {
-		DummyListener dummy = new DummyListener();
-		reader.addSentenceListener(dummy);
-		reader.removeSentenceListener(dummy);
+		assertFalse(started);
+		reader.removeSentenceListener(testListener);
+		reader.fireReadingStarted();
+		assertFalse(started);
 	}
 
 	@Test
@@ -79,49 +88,87 @@ public class SentenceReaderTest {
 	}
 
 	@Test
-	public void testStart() {
-		reader.start();
-	}
-
-	@Test
-	public void testStop() {
-		try {
-			reader.start();
-			Thread.sleep(1000);
-			reader.stop();
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
-
-	@Test
 	@Ignore
 	public void testFireReadingPaused() {
-		fail("Not yet implemented");
+		assertFalse(paused);
+		reader.fireReadingPaused();
+		assertTrue(paused);
 	}
 
 	@Test
 	@Ignore
 	public void testFireReadingStarted() {
-		fail("Not yet implemented");
+		assertFalse(started);
+		reader.fireReadingStarted();
+		assertTrue(started);
 	}
 
 	@Test
 	@Ignore
 	public void testFireReadingStopped() {
-		fail("Not yet implemented");
+		assertFalse(stopped);
+		reader.fireReadingStopped();
+		assertTrue(stopped);
 	}
 
 	@Test
-	@Ignore
-	public void testFireSentenceEvent() {
-		fail("Not yet implemented");
+	public void testFireSentenceEventWithExpectedType() {
+		assertNull(sentence);
+		SentenceFactory sf = SentenceFactory.getInstance();
+		Sentence s = sf.createParser(GGATest.EXAMPLE);
+		reader.fireSentenceEvent(s);
+		assertEquals(s, sentence);
 	}
-		
+
+	@Test
+	public void testFireSentenceEventWithUnexpectedType() {
+		assertNull(sentence);
+		SentenceFactory sf = SentenceFactory.getInstance();
+		Sentence s = sf.createParser(BODTest.EXAMPLE);
+		reader.fireSentenceEvent(s);
+		assertNull(sentence);
+	}
+	
+	@Test
+	public void testStartAndStop() {
+		try {
+			assertNull(sentence);
+			assertFalse(started);
+			assertFalse(paused);
+			assertFalse(stopped);
+			
+			reader.start();
+			Thread.sleep(500);
+			
+			assertNotNull(sentence);
+			assertTrue(started);
+			assertFalse(paused);
+			assertFalse(stopped);
+			
+			reader.stop();
+			Thread.sleep(100);
+			
+			assertFalse(paused);
+			assertTrue(stopped);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
 	public class DummyListener implements SentenceListener {
 		public void readingPaused() {}
 		public void readingStarted() {}
 		public void readingStopped() {}
 		public void sentenceRead(SentenceEvent event) {}
 	}
+
+	public class TestListener implements SentenceListener {
+		public void readingPaused() { paused = true; }
+		public void readingStarted() { started = true; }
+		public void readingStopped() { stopped = true; }
+		public void sentenceRead(SentenceEvent event) {
+			sentence = event.getSentence();
+		}
+	}
+	
 }
