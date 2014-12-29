7bf67f0fdce5080ed2a6b8c81d21f649ad364602
Forsety <forsssety@gmail.com>
2014-12-29 16:00:09 +0200

    Fix month index when converting Date to java.util.Date

--- src/main/java/net/sf/marineapi/nmea/util/Date.java
+++ src/main/java/net/sf/marineapi/nmea/util/Date.java
@@ -238,7 +238,7 @@ public class Date {
 	public java.util.Date toDate() {
 		GregorianCalendar cal = new GregorianCalendar();
 		cal.set(Calendar.YEAR, getYear());
-		cal.set(Calendar.MONTH, getMonth());
+		cal.set(Calendar.MONTH, getMonth() - 1);
 		cal.set(Calendar.DAY_OF_MONTH, getDay());
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
