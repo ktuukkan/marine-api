064e5597e873bd01b22f9e40a68ecc8ecb8adfb8
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2015-01-29 08:37:17 +0200

    Reverted FileExample changes.

--- src/main/java/net/sf/marineapi/example/FileExample.java
+++ src/main/java/net/sf/marineapi/example/FileExample.java
@@ -25,26 +25,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.sf.marineapi.ais.parser.AISMessage;
-import net.sf.marineapi.ais.parser.AISMessage01Parser;
-import net.sf.marineapi.ais.parser.AISMessage02Parser;
-import net.sf.marineapi.ais.parser.AISMessage03Parser;
-import net.sf.marineapi.ais.parser.AISMessage04Parser;
-import net.sf.marineapi.ais.parser.AISMessage05Parser;
-import net.sf.marineapi.ais.parser.AISMessage18Parser;
-import net.sf.marineapi.ais.parser.AISMessage19Parser;
-import net.sf.marineapi.ais.util.MMSI;
-import net.sf.marineapi.ais.util.Violation;
 import net.sf.marineapi.nmea.event.SentenceEvent;
 import net.sf.marineapi.nmea.event.SentenceListener;
 import net.sf.marineapi.nmea.io.SentenceReader;
 import net.sf.marineapi.nmea.sentence.GGASentence;
 import net.sf.marineapi.nmea.sentence.SentenceId;
-import net.sf.marineapi.nmea.sentence.VDMSentence;
 
 /**
  * Simple example application that takes a filename as command-line argument and
- * prints Position from received GGA and VDM sentences.
+ * prints Position from received GGA sentences.
  * 
  * @author Kimmo Tuukkanen
  */
@@ -65,8 +54,6 @@ public class FileExample implements SentenceListener {
 
 		// register self as a listener for GGA sentences
 		reader.addSentenceListener(this, SentenceId.GGA);
-		// and also for all AIS messages
-		reader.addSentenceListener(this, SentenceId.VDM);
 		reader.start();
 	}
 
@@ -102,56 +89,14 @@ public class FileExample implements SentenceListener {
 	 */
 	public void sentenceRead(SentenceEvent event) {
 
-		// When you are registered only for a single sentence we can safely cast
-		// it directly.
-		// When receiving multiple sentences without filtering, you should check the
+		// Safe to cast as we are registered only for GGA updates. Could
+		// also cast to PositionSentence if interested only in position data.
+		// When receiving all sentences without filtering, you should check the
 		// sentence type before casting (e.g. with Sentence.getSentenceId()).
-		if (event.getSentence().getSentenceId().equals("GGA")) {
-			GGASentence s = (GGASentence) event.getSentence();
+		GGASentence s = (GGASentence) event.getSentence();
 
-			// Do something with sentence data..
-			System.out.println(s.getPosition());
-		}
-		else if (event.getSentence().getSentenceId().equals("VDM")) {
-			VDMSentence s = (VDMSentence) event.getSentence();
-			try {
-				AISMessage mes = null;
-				switch (s.getMessageType()) {
-				case 1:
-					mes = new AISMessage01Parser(s.getMessageBody());
-					break;
-				case 2:
-					mes = new AISMessage02Parser(s.getMessageBody());
-					break;
-				case 3:
-					mes = new AISMessage03Parser(s.getMessageBody());
-					break;
-				case 4:
-					mes = new AISMessage04Parser(s.getMessageBody());
-					break;
-				case 5:
-					mes = new AISMessage05Parser(s.getMessageBody());
-					break;
-				case 18:
-					mes = new AISMessage18Parser(s.getMessageBody());
-					break;
-				case 19:
-					mes = new AISMessage19Parser(s.getMessageBody());
-					break;
-				}
-				if (mes != null) {
-					System.out.println("AIS" + s.getMessageType() +
-							" MMSI: " + s.getMMSI() + " " + MMSI.toString(s.getMMSI()));
-					System.out.println("Violations: " + mes.getNrOfViolations());
-					if (mes.getNrOfViolations() > 0) {
-						for (Violation v : mes.getViolations())
-							System.out.println("\t" + v.toString());
-					}
-					System.out.println(mes.toString());
-				}
-			}
-			catch (Exception e) {}
-		}
+		// Do something with sentence data..
+		System.out.println(s.getPosition());
 	}
 
 	/**
