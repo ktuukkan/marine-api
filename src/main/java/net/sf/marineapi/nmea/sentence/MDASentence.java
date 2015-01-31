2d1e9a74ff7927622b5be5bff7da408bbf2887f0
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2015-01-31 10:16:19 +0200

    Copyright/license comments in MDA and MWD classes.

--- src/main/java/net/sf/marineapi/nmea/sentence/MDASentence.java
+++ src/main/java/net/sf/marineapi/nmea/sentence/MDASentence.java
@@ -1,3 +1,23 @@
+/* 
+ * MDASentence.java
+ * Copyright (C) 2015 INDI for Java NMEA 0183 stream driver
+ * 
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ * 
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.nmea.sentence;
 
 import net.sf.marineapi.nmea.sentence.Sentence;
@@ -8,7 +28,6 @@ import net.sf.marineapi.nmea.sentence.Sentence;
  * of the earth.
  * 
  * @author Richard van Nieuwenhoven
- *
  */
 public interface MDASentence extends Sentence {
 
--- src/main/java/net/sf/marineapi/nmea/sentence/MWDSentence.java
+++ src/main/java/net/sf/marineapi/nmea/sentence/MWDSentence.java
@@ -1,31 +1,29 @@
+/*
+ * MWDSentence.java
+ * Copyright (C) 2015 INDI for Java NMEA 0183 stream driver
+ *  
+ * This file is part of Java Marine API.
+ * <http://ktuukkan.github.io/marine-api/>
+ * 
+ * Java Marine API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Java Marine API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.marineapi.nmea.sentence;
 
-/*
- * #%L
- * INDI for Java NMEA 0183 stream driver
- * %%
- * Copyright (C) 2012 - 2015 indiforjava
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
 /**
  * Wind speed and direction.
  * 
  * @author Richard van Nieuwenhoven
- *
  */
 public interface MWDSentence {
 
