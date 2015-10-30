84d340a6bb29cf8cefcdc7d34ccb276c2a615731
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2015-10-30 16:59:00 +0200

    organized imports

--- src/test/java/net/sf/marineapi/nmea/io/SentenceReaderTest.java
+++ src/test/java/net/sf/marineapi/nmea/io/SentenceReaderTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import net.sf.marineapi.nmea.event.SentenceEvent;
