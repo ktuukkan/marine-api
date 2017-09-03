b2ab92bae6486363eafbee7987b51a86be27f8e4
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2017-09-03 17:07:51 +0300

    allow setting negative rate of turn values in ROTParser (issue #74)

--- src/main/java/net/sf/marineapi/nmea/parser/ROTParser.java
+++ src/main/java/net/sf/marineapi/nmea/parser/ROTParser.java
@@ -1,20 +1,20 @@
 /*
  * ROTParser.java
  * Copyright (C) 2014 Mike Tamis, Kimmo Tuukkanen
- * 
+ *
  * This file is part of Java Marine API.
  * <http://ktuukkan.github.io/marine-api/>
- * 
+ *
  * Java Marine API is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * Java Marine API is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ import net.sf.marineapi.nmea.util.DataStatus;
 
 /**
  * ROT sentence parser.
- * 
+ *
  * @author Mike Tamis, Kimmo Tuukkanen
  */
 class ROTParser extends SentenceParser implements ROTSentence {
@@ -37,7 +37,7 @@ class ROTParser extends SentenceParser implements ROTSentence {
 
 	/**
 	 * Creates a new ROT parser.
-	 * 
+	 *
 	 * @param nmea ROT sentence String to parse.
 	 */
 	public ROTParser(String nmea) {
@@ -46,7 +46,7 @@ class ROTParser extends SentenceParser implements ROTSentence {
 
 	/**
 	 * Creates a new empty ROT sentence.
-	 * 
+	 *
 	 * @param talker Talker id to set
 	 */
 	public ROTParser(TalkerId talker) {
@@ -55,7 +55,7 @@ class ROTParser extends SentenceParser implements ROTSentence {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see net.sf.marineapi.nmea.parser.RateOfTurnSentance#getRateOfTurn()
 	 */
 	public double getRateOfTurn() {
@@ -64,7 +64,7 @@ class ROTParser extends SentenceParser implements ROTSentence {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see net.sf.marineapi.nmea.sentence.RateOfTurnSentance#getStatus()
 	 */
 	public DataStatus getStatus() {
@@ -73,16 +73,16 @@ class ROTParser extends SentenceParser implements ROTSentence {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see net.sf.marineapi.nmea.sentence.ROTSentence#setRateOfTurn(double)
 	 */
 	public void setRateOfTurn(double rot) {
-		setDegreesValue(RATE_OF_TURN, rot);
+		setDoubleValue(RATE_OF_TURN, rot, 3, 1);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see
 	 * net.sf.marineapi.nmea.sentence.ROTSentence#setStatus(net.sf.marineapi
 	 * .nmea.util.DataStatus)
