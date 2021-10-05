3cf94c41c4f2e2f431357b0585bdaf55cca4021f
Kimmo Tuukkanen <kimmo.tuukkanen@gmail.com>
2021-10-05 21:51:56 +0300

    Add RTK_FIXED and RTK_FLOAT to FaaMode enum.

--- src/main/java/net/sf/marineapi/nmea/util/FaaMode.java
+++ src/main/java/net/sf/marineapi/nmea/util/FaaMode.java
@@ -1,20 +1,20 @@
-/* 
+/*
  * FaaMode.java
  * Copyright (C) 2010 Kimmo Tuukkanen
- * 
+ *
  * This file is part of Java Marine API.
  * <http://ktuukkan.github.io/marine-api/>
- * 
+ *
  * Java Marine API is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * Java Marine API is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,7 +28,7 @@ package net.sf.marineapi.nmea.util;
  * Notice that FAA mode dominates the {@link DataStatus} fields. Status field
  * will be set to {@link DataStatus#ACTIVE} for modes {@link #AUTOMATIC} and
  * {@link #DGPS}, and {@link DataStatus#VOID} for all other modes.</p>
- * 
+ *
  * @author Kimmo Tuukkanen
  * @see GpsFixQuality
  * @see GpsFixStatus
@@ -51,6 +51,10 @@ public enum FaaMode {
 	/** Operating in precise mode, no degradation like Selective Availability (NMEA 4.00 and later). */
 	PRECISE('P'),
 
+	RTK_FIXED('R'),
+
+	RTK_FLOAT('F'),
+
 	/** Simulated data (running in simulator/demo mode) */
 	SIMULATED('S'),
 
@@ -65,7 +69,7 @@ public enum FaaMode {
 
 	/**
 	 * Returns the corresponding char indicator of GPS mode.
-	 * 
+	 *
 	 * @return Mode char used in sentences.
 	 */
 	public char toChar() {
@@ -75,7 +79,7 @@ public enum FaaMode {
 	/**
 	 * Returns the FaaMode enum corresponding the actual char indicator used in
 	 * the sentencess.
-	 * 
+	 *
 	 * @param ch Char mode indicator
 	 * @return FaaMode enum
 	 */
